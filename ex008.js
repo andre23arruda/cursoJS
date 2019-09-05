e7fe687281ad6b729acbee595de0eb036f823ec8
André Arruda <33252657+andre23arruda@users.noreply.github.com>
2019-09-04 22:37:42 -0300

    Delete ex008.js

--- ex008.js
+++
@@ -1,7 +0,0 @@
-console.log('Olá, mundo!')
-var vel = 12
-console.log(`A velocidade do seu carro é ${vel}`)
-if (vel>60){
-    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
-}
-console.log(`Dirija sempre usando cinto de segurança!`)
